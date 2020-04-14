345bf002a7b3ae831ba35305f057c004ce087c71
Christian Bremer <bremersee@googlemail.com>
2020-04-14 20:57:43 +0200

    Init

--- src/main/java/org/bremersee/adminserver/Application.java
+++ src/main/java/org/bremersee/adminserver/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
