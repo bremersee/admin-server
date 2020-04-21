c7cda78341b0e2b4504485725a586f017970e32c
Christian Bremer <bremersee@googlemail.com>
2020-04-21 17:12:15 +0200

    Init

--- src/main/java/org/bremersee/adminserver/config/SecurityConfiguration.java
+++ src/main/java/org/bremersee/adminserver/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package org.bremersee.adminserver.config;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
 import org.bremersee.security.authentication.AuthenticationProperties;
+import org.bremersee.security.authentication.PasswordFlowAuthenticationManager;
+import org.bremersee.security.core.AuthorityConstants;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -18,30 +20,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Assert;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
-  /*
-  @ConditionalOnProperty(
-      prefix = "bremersee.security.authentication",
-      name = "enable-jwt-support",
-      havingValue = "true")
-  @Configuration
-  @Slf4j
-  @EnableConfigurationProperties(AuthenticationProperties.class)
-  static class PasswordFlowAuthentication extends WebSecurityConfigurerAdapter {
-
-  }
-  */
 
   @ConditionalOnProperty(
       prefix = "bremersee.security.authentication",
       name = "enable-jwt-support",
       havingValue = "false", matchIfMissing = true)
   @Configuration
-  @Slf4j
   @EnableConfigurationProperties(AuthenticationProperties.class)
   static class SimpleUserAuthentication extends WebSecurityConfigurerAdapter {
 
@@ -56,32 +45,44 @@ public class SecurityConfiguration {
       this.properties = properties;
     }
 
+    @SuppressWarnings("DuplicatedCode")
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+      SavedRequestAwareAuthenticationSuccessHandler successHandler
+          = new SavedRequestAwareAuthenticationSuccessHandler();
       successHandler.setTargetUrlParameter("redirectTo");
       successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
 
       http
           .authorizeRequests((authorizeRequests) -> authorizeRequests
-              .antMatchers(this.adminServer.path("/assets/**")).permitAll()
-              .antMatchers(this.adminServer.path("/login")).permitAll()
+              .antMatchers(adminServer.path("/assets/**")).permitAll()
+              .antMatchers(adminServer.path("/login")).permitAll()
               .antMatchers(adminServer.path("/actuator/health")).permitAll()
               .antMatchers(adminServer.path("/actuator/info")).permitAll()
-              .antMatchers(adminServer.path("/actuator/**")).access(properties.getActuator().buildAccessExpression())
-              .anyRequest().authenticated())
+              .antMatchers(HttpMethod.OPTIONS, adminServer.path("/actuator/**")).permitAll()
+              .antMatchers(HttpMethod.GET, adminServer.path("/actuator/**"))
+              .access(properties.getActuator()
+                  .buildAccessExpression(properties::ensureRolePrefix))
+              .antMatchers(adminServer.path("/actuator/**"))
+              .access(properties.getActuator()
+                  .buildAdminAccessExpression(properties::ensureRolePrefix))
+              .anyRequest()
+              .access(properties.getApplication().buildAccessExpression(
+                  false, false, false, true,
+                  properties::ensureRolePrefix,
+                  AuthorityConstants.ADMIN_ROLE_NAME)))
           .formLogin((formLogin) -> formLogin
-              .loginPage(this.adminServer.path("/login")).successHandler(successHandler)
+              .loginPage(adminServer.path("/login")).successHandler(successHandler)
               .and())
-          .logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout")))
+          .logout((logout) -> logout.logoutUrl(adminServer.path("/logout")))
           .httpBasic(Customizer.withDefaults())
           .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
               .ignoringRequestMatchers(
-                  new AntPathRequestMatcher(this.adminServer.path("/instances"),
+                  new AntPathRequestMatcher(adminServer.path("/instances"),
                       HttpMethod.POST.toString()),
-                  new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
+                  new AntPathRequestMatcher(adminServer.path("/instances/*"),
                       HttpMethod.DELETE.toString()),
-                  new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
+                  new AntPathRequestMatcher(adminServer.path("/actuator/**"))
               ))
           .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString())
               .tokenValiditySeconds(1209600));
@@ -93,6 +94,77 @@ public class SecurityConfiguration {
       return new InMemoryUserDetailsManager(properties.buildBasicAuthUserDetails());
     }
 
+  }
+
+  @ConditionalOnProperty(
+      prefix = "bremersee.security.authentication",
+      name = "enable-jwt-support",
+      havingValue = "true")
+  @Configuration
+  @EnableConfigurationProperties(AuthenticationProperties.class)
+  static class PasswordFlowAuthentication extends WebSecurityConfigurerAdapter {
+
+    private AdminServerProperties adminServer;
+
+    private AuthenticationProperties properties;
+
+    private PasswordFlowAuthenticationManager passwordFlowAuthenticationManager;
+
+    public PasswordFlowAuthentication(
+        AdminServerProperties adminServer,
+        AuthenticationProperties properties,
+        ObjectProvider<PasswordFlowAuthenticationManager> passwordFlowAuthenticationManager) {
+      this.adminServer = adminServer;
+      this.properties = properties;
+      this.passwordFlowAuthenticationManager = passwordFlowAuthenticationManager.getIfAvailable();
+      Assert.notNull(
+          this.passwordFlowAuthenticationManager,
+          "Password flow authentication manager must be present.");
+    }
+
+    @SuppressWarnings("DuplicatedCode")
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      SavedRequestAwareAuthenticationSuccessHandler successHandler
+          = new SavedRequestAwareAuthenticationSuccessHandler();
+      successHandler.setTargetUrlParameter("redirectTo");
+      successHandler.setDefaultTargetUrl(adminServer.path("/"));
+
+      http
+          .authorizeRequests((authorizeRequests) -> authorizeRequests
+              .antMatchers(adminServer.path("/assets/**")).permitAll()
+              .antMatchers(adminServer.path("/login")).permitAll()
+              .antMatchers(adminServer.path("/actuator/health")).permitAll()
+              .antMatchers(adminServer.path("/actuator/info")).permitAll()
+              .antMatchers(HttpMethod.OPTIONS, adminServer.path("/actuator/**")).permitAll()
+              .antMatchers(HttpMethod.GET, adminServer.path("/actuator/**"))
+              .access(properties.getActuator()
+                  .buildAccessExpression(properties::ensureRolePrefix))
+              .antMatchers(adminServer.path("/actuator/**"))
+              .access(properties.getActuator()
+                  .buildAdminAccessExpression(properties::ensureRolePrefix))
+              .anyRequest()
+              .access(properties.getApplication().buildAccessExpression(
+                  false, false, false, true,
+                  properties::ensureRolePrefix,
+                  AuthorityConstants.ADMIN_ROLE_NAME)))
+          .authenticationProvider(passwordFlowAuthenticationManager)
+          .formLogin((formLogin) -> formLogin
+              .loginPage(adminServer.path("/login")).successHandler(successHandler)
+              .and())
+          .logout((logout) -> logout.logoutUrl(adminServer.path("/logout")))
+          .httpBasic(Customizer.withDefaults())
+          .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+              .ignoringRequestMatchers(
+                  new AntPathRequestMatcher(adminServer.path("/instances"),
+                      HttpMethod.POST.toString()),
+                  new AntPathRequestMatcher(adminServer.path("/instances/*"),
+                      HttpMethod.DELETE.toString()),
+                  new AntPathRequestMatcher(adminServer.path("/actuator/**"))
+              ))
+          .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString())
+              .tokenValiditySeconds(1209600));
+    }
   }
 
 }
--- src/test/java/org/bremersee/adminserver/ApplicationTests.java
+++ src/test/java/org/bremersee/adminserver/ApplicationTests.java
@@ -1,13 +1,70 @@
 package org.bremersee.adminserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+    "bremersee.security.authentication.enable-jwt-support=false"
+})
+@ActiveProfiles({"basic-auth"})
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  private static final String actuatorUser = "actuator";
+
+  private static final String actuatorPass = "actuator";
+
+  /**
+   * The rest template.
+   */
+  @Autowired
+  TestRestTemplate restTemplate;
+
+  /**
+   * Fetch health.
+   */
+  @Test
+  void fetchHealth() {
+    ResponseEntity<String> response = restTemplate
+        .getForEntity("/actuator/health", String.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  /**
+   * Fetch info.
+   */
+  @Test
+  void fetchInfo() {
+    ResponseEntity<String> response = restTemplate
+        .getForEntity("/actuator/info", String.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  /**
+   * Fetch metrics.
+   */
+  @Test
+  void fetchMetrics() {
+    ResponseEntity<String> response = restTemplate
+        .withBasicAuth(actuatorUser, actuatorPass)
+        .getForEntity("/actuator/metrics", String.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  /**
+   * Fetch metrics and expect status 302 (because form login s enabled).
+   */
+  @Test
+  void fetchMetricsAndExpect302() {
+    ResponseEntity<String> response = restTemplate
+        .getForEntity("/actuator/metrics", String.class);
+    assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
 
 }
